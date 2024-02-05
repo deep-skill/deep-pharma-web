efce53f7c4873645d127ca28c1bb52e00f98c3d4
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-05 12:05:32 -0500

    add global_state ,  colors and theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,21 @@ const config: Config  = withMT({
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
