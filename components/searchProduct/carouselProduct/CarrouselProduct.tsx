a6c760b9923839b5ce723c90d05957f3c6c159f7
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-20 14:56:17 -0500

    change text size

--- components/searchProduct/carouselProduct/CarrouselProduct.tsx
+++ components/searchProduct/carouselProduct/CarrouselProduct.tsx
@@ -4,16 +4,16 @@ import { IoMdAddCircleOutline } from "react-icons/io"
 
 const CarouselProduct = () => {
     return (
-        <div className="flex flex-col rounded-lg shadow-lg p-4">
-            <h3 className="text-3xl font-bold text-product">Medicamentos</h3>
-            <p className="text-product">S/ 2.5</p>
+        <div className="flex flex-col w-46 rounded-lg shadow-lg p-4">
+            <h3 className="text-lg font-bold text-product">Medicamentos</h3>
+            <p className="text-product text-sm">S/ 2.5</p>
             <div className="flex gap-2 justify-between items-center">
-                <p className="text-product">Farmaindrustia</p>
-                <p className="text-product">250 mg</p>
+                <p className="text-product text-xs">Farmaindrustia</p>
+                <p className="text-product text-xs">250 mg</p>
             </div>
-            <p className="text-product">15 unidades den stock</p>
+            <p className="text-product text-sm">15 unidades den stock</p>
             <div className="flex gap-2 justify-between items-center">
-                <Link className="text-white bg-orange p-2 rounded-lg" href={'/'}>Ver detalles</Link>
+                <Link className="text-white text-xs bg-orange p-1 rounded-md" href={'/'}>Ver detalles</Link>
                 <IoMdAddCircleOutline style={{fontSize: '2rem'}}/>
             </div>
         </div>
