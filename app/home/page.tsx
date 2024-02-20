c0863949733351e37e70bf9e629bffe812aff254
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-20 13:15:32 -0500

    add gettoken and change home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,34 +1,12 @@
-'use client';
-import { useEffect } from 'react';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { fetchToken } from '@/lib/fetch/fetchToken';
 import Carousel from '@/components/carousel/Carousel';
+import GetToken from '@/components/GetToken';
 
 const HomePage = () => {
-  const { user, error, isLoading } = useUser();
-
-  useEffect(() => {
-    const getToken = async () => {
-      if (user) {
-        try {
-          await fetchToken();
-        } catch (err) {
-          console.error('Error al obtener el token:', err);
-        }
-      }
-    };
-
-    getToken();
-  }, [user]);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
   return (
     <div>
-      <div>{user?.name}</div>
-      <div>{user?.email}</div>
+      <GetToken/>
       <Carousel />
+      
     </div>
   );
 };
