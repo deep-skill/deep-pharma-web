bda17c9f64aee40eddfd67216bfbc4cec92dcc3d
brunoosuna8 <brunoosuna8@gmail.com>
2024-02-21 10:15:54 -0300

    minor change

--- components/section-button/SectionButton.tsx
+++ components/section-button/SectionButton.tsx
@@ -15,7 +15,7 @@ const SectionButton = ({ title, subTitle, image, secImage, color }: Props) => {
       className={`w-11/12  ${color}  rounded-2xl text-white font-bold flex justify-between items-center h-28 m-auto my-8`}
     >
       <div className="w-7/12 md:w-8/12 lg:w-10/12 h-full flex flex-col justify-center px-5 py-10">
-        <p className="sm:text-md md:text-lg lg:text-3xl ">{title}</p>
+        <p className="text-lg sm:text-lg md:text-lg lg:text-3xl ">{title}</p>
         <p className="text-xs sm:text-sm md:text-md lg:text-lg font-light">
           {subTitle}
         </p>
