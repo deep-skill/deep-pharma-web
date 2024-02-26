3adb10df15532ff7bc808a3052bfb512b53e0fa0
Franco Galbiati <francogalbiati984@gmail.com>
2024-02-26 11:09:45 -0500

    minor change

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import Profile from "@/components/Profile";
 import UserProfile from "@/components/user-profil/UserProfil";
 
 const UserProfilePage = () => {
